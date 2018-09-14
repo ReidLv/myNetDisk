497e0f2ad462a8d962565c0429c7de8a5ef88c6d
deyi.lv <deyi.lv@cy-tech.net>
2018-09-14 16:52:47 +0800

    first commit

---
+++ app/schedulers/logio/logio.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 9/6/18 11:02 AM
+# @Author  : Reid
+# @Site    : 
+# @File    : logio.py
+# @Software: PyCharm
+# @license : Copyright(C), ChangYang Technology Co. Ltd.
+from flask import request, render_template
+from flask_login import login_user, logout_user, current_user
+
