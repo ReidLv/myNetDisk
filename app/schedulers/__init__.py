71b289a1860ed55bd183589ec623a9c793c01aa2
deyi.lv <deyi.lv@cy-tech.net>
2018-09-14 19:38:15 +0800

    update .gitignore

--- app/schedulers/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 9/5/18 6:27 PM
-# @Author  : Reid
-# @Site    : 
-# @File    : __init__.py
-# @Software: PyCharm
-# @license : Copyright(C), ChangYang Technology Co. Ltd.
