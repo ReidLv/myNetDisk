1062d540de6160147ddfd314dcc7554d797b9620
ReidLv <33021142+ReidLv@users.noreply.github.com>
2018-11-01 15:08:41 +0800

    编码文件二进制流为base64字符串,后端解码保存(backend实现,frontend待添加)

--- app/schedulers/filemgr/filemgr.py
+++ app/schedulers/filemgr/filemgr.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import json
+from base64 import b64encode, b64decode
 from flask import request, render_template, jsonify, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from app.schedulers.filemgr import filemgr_blueprint as filemgr
-import os
 
 
 @filemgr.route('/')
@@ -35,3 +37,15 @@ def upload():
     file = request.files['file']
     file.save(os.getcwd()+'/files/'+file.filename)
     return render_template('filemgr.html')
+
+@filemgr.route('/imgupload', methods=['POST'])
+@login_required
+def image_upload():
+    data = json.loads(request.get_data())
+    imgStrB64 = data.get('imgStrB64')
+    imgName = data.get('fileName')
+    if not all([imgStrB64, imgName]):
+        return jsonify(status=0)
+    with open(os.getcwd()+'/files/'+imgName, 'wb') as f:
+        f.write(b64decode(imgStrB64))
+    return jsonify(status=1)
